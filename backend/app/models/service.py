7c6f93df5af313c86decc3554fedb2d5499e8a0c
Mfys212 <techpy212@gmail.com>
2026-01-21 20:09:03 +0700

    update db: table service

--- backend/app/models/service.py
+++ backend/app/models/service.py
@@ -9,7 +9,6 @@ class Service(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(255), nullable=False)
     desc = Column(Text, nullable=False)
-    link = Column(String(255), nullable=False)
     icon = Column(String(255), nullable=False)
     created_at = Column(
         DateTime(timezone=True),
