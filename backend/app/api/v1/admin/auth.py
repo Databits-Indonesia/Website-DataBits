98fec8c9f787951a26604d3f9677402e6a4663fe
Mfys212 <techpy212@gmail.com>
2026-02-01 10:33:05 +0700

    login user

--- backend/app/api/v1/admin/auth.py
+++ backend/app/api/v1/admin/auth.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, status, Response
 from sqlalchemy.orm import Session
 from app.schemas.auth import LoginRequest, TokenResponse, UserMe
 from app.core.database import get_db
+from app.services.auth import login_user
 from app.core.security import ACCESS_TOKEN_EXPIRE_MINUTES
 from app.core.dependencies import get_current_user
 
@@ -9,7 +10,6 @@ router = APIRouter(prefix="/auth", tags=["Auth"])
 
 @router.post("/login", response_model=TokenResponse)
 def login(data: LoginRequest, response: Response, db: Session = Depends(get_db)):
-    from app.services.auth import login_user
     token = login_user(db, data.email_username, data.password)
 
     if not token:
