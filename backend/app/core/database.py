ef079e0ee6e22673fc0ab5382101dd98e99d476c
nafamubarokhusni <nafamubarokhusni@gmail.com>
2026-05-12 23:24:53 +0700

    feat: initialize database configuration and SQLAlchemy session management

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,7 +1,11 @@
+import pymysql  # Add this import
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 from app.core.config import DATABASE_URL
+
+# This line is the "magic" that fixes the MySQLdb error
+pymysql.install_as_MySQLdb() 
 
 engine = create_engine(
     DATABASE_URL,
@@ -15,7 +19,6 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
-
 
 def get_db():
     db = SessionLocal()
